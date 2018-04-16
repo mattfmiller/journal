5d6a8af65fe39d9facfcce05369fe7a1b87d852c
Cory West-Forbes and Matt Miller <cory@>
2018-04-16 09:56:58 -0700

    Add linter, plugins, frontend js to main.js.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import {Entry} from './word-count.js';
+import './styles.css';
+
+
+$(function(){
+  $('#journal-form').submit(function(event){
+    var userBodyInput = $('#body-input').val();
+    var userTitleInput = $('#title-input').val();
+    var journalEntry = new Entry(userTitleInput, userBodyInput);
+
+    var userWordCount = journalEntry.countWords();
+
+    $('#show-word-count').text(userWordCount);
+    event.preventDefault();
+  });
+});
